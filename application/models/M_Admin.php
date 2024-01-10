b6132202e7b5bb0b558269979d50b2aeb44b393e
21-098bagus <89884845+21-098bagus@users.noreply.github.com>
2024-01-10 09:11:18 +0700

    first commit

---
+++ application/models/M_Admin.php
@@ -0,0 +1,199 @@
+<?php if(! defined('BASEPATH')) exit('No direct script acess allowed');
+
+class M_Admin extends CI_Model
+{
+  function __construct()
+  {
+	 parent::__construct();
+	 //validasi jika user belum login
+	 }
+
+   function get_table($table_name)
+   {
+     $get_user = $this->db->get($table_name);
+     return $get_user->result_array();
+   }
+
+   function get_tableid($table_name,$where,$id)
+   {
+     $this->db->where($where,$id);
+     $edit = $this->db->get($table_name);
+     return $edit->result_array();
+   }
+
+   function get_tableid_edit($table_name,$where,$id)
+   {
+     $this->db->where($where,$id);
+     $edit = $this->db->get($table_name);
+     return $edit->row();
+   }
+
+   function add_multiple($table,$data = array())
+  {
+      $total_array = count($data);
+
+      if($total_array != 0)
+      {
+      $this->db->insert_batch($table, $data);
+      }
+  }
+
+   function insertTable($table_name,$data)
+   {
+     $tambah = $this->db->insert($table_name,$data);
+     return $tambah;
+   }
+
+   function LastinsertId($table_name,$data)
+   {
+     $this->db->insert($table_name,$data);
+     $insert_id = $this->db->insert_id();
+     return $insert_id;
+   }
+
+   function update_table($table_name,$where,$id,$data)
+   {
+     $this->db->where($where,$id);
+     $update = $this->db->update($table_name,$data);
+     return $update;
+   }
+
+   function delete_table($table_name,$where,$id)
+   {
+     $this->db->where($where,$id);
+     $hapus = $this->db->delete($table_name);
+     return $hapus;
+   }
+
+   function delete_table_multiple($table_name,$where,$id)
+   {
+      if (!empty($id)) {
+         $this->db->where_in($where,$id);
+         $hapus = $this->db->delete($table_name);
+         return $hapus;
+      }
+   }
+
+   function edit_table($table_name,$where,$id)
+   {
+     $this->db->where($where,$id);
+     $edit = $this->db->get($table_name);
+     return $edit->row();
+   }
+
+   function CountTable($table_name)
+   {
+     $Count = $this->db->get($table_name);
+     return $Count->num_rows();
+   }
+
+   function CountTableId($table_name,$where,$id)
+   {
+     $this->db->where($where,$id);
+     $Count = $this->db->get($table_name);
+     return $Count->num_rows();
+   }
+
+   function SelectTable($table_name,$query,$id,$orderby)
+   {
+       $this->db->select($query, FALSE); // select('RIGHT(user.id_odojers,4) as kode', FALSE);
+       $this->db->order_by($id,$orderby);
+       $query = $this->db->get($table_name); // cek dulu apakah ada sudah ada kode di tabel.
+       return $query;
+   }
+
+   function SelectTableSQL($query)
+   {
+       $row = $this->db->query($query);
+       return $row;
+   }
+
+  function get_user($user)
+  {
+    $this->db->where('id_login',$user);
+    $get_user = $this->db->get('tbl_login');
+    return $get_user->row();
+	}
+	
+	function rp($angka){
+			$hasil_rupiah = "Rp" . number_format($angka,0,',','.'). ',-';
+			return $hasil_rupiah;
+	}
+
+ 
+	public function buat_kode($table_name,$kodeawal,$idkode,$orderbylimit)
+  {
+      $query = $this->db->query("select * from $table_name $orderbylimit"); // cek dulu apakah ada sudah ada kode di tabel.
+      
+		  if($query->num_rows() > 0){
+        //jika kode ternyata sudah ada.
+        $hasil = $query->row();
+        $kd = $hasil->$idkode;
+        $cd = $kd;
+        $nomor = $query->num_rows();
+        $kode = $cd + 1;
+        $kodejadi = $kodeawal."00".$kode;    // hasilnya CUS-0001 dst.
+        $kdj = $kodejadi;
+		  }else {
+        //jika kode belum ada
+        $kode = 0+1;
+        $kodejadi = $kodeawal."00".$kode;    // hasilnya CUS-0001 dst.
+        $kdj = $kodejadi;
+      }
+		  return $kdj;
+  }
+
+  public function buat_kode_join($table_name,$kodeawal,$idkode)
+  {
+      $query = $this->db->query($table_name); // cek dulu apakah ada sudah ada kode di tabel.
+		  if($query->num_rows() > 0){
+        //jika kode ternyata sudah ada.
+        $hasil = $query->row();
+        $kd = $hasil->$idkode;
+        $cd = $kd;
+        $kode = $cd + 1;
+        $kodejadi = $kodeawal."00".$kode;    // hasilnya CUS-0001 dst.
+        $kdj = $kodejadi;
+		  }else {
+        //jika kode belum ada
+        $kode = 0+1;
+        $kodejadi = $kodeawal."00".$kode;    // hasilnya CUS-0001 dst.
+        $kdj = $kodejadi;
+      }
+		  return $kdj;
+  }
+  
+  function acak($panjang)
+  {
+      $karakter= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
+      $string = '';
+      for ($i = 0; $i < $panjang; $i++) {
+        $pos = rand(0, strlen($karakter)-1);
+        $string .= $karakter[$pos];
+      }
+      return $string;
+  }
+  public function detailpinjam()
+{
+    // ... Bagian kode sebelumnya ...
+
+    // Ambil data nomor seri dari tbl_pinjam
+    $serial_numbers = $this->M_Admin->get_serial_numbers($id);
+
+    // Melewatkan data nomor seri ke tampilan
+    $this->data['serial_num'] = $serial_num;
+
+    // ... Bagian kode selanjutnya ...
+}
+
+public function get_serial_numbers($pinjam_id)
+{
+    // Ambil data nomor seri berdasarkan pinjam_id dari tbl_pinjam
+    $query = $this->db->get_where('tbl_pinjam', array('pinjam_id' => $pinjam_id));
+
+    // Mengembalikan hasil dalam bentuk array objek
+    return $query->result();
+}
+
+}
+?>
